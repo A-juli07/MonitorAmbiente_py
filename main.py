938f353f05a61d5905d6d738c4e16bea2515eae6
Ana_Julia_Costa <113532608+A-juli07@users.noreply.github.com>
2024-10-29 18:20:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,28 +21,16 @@ def conecta_wifi():
 conecta_wifi()
 
 sensor = dht.DHT11(machine.Pin(4))
-RELE_PIN = machine.Pin(17, machine.Pin.OUT)
-RELE_PIN.value(0)
 
 def thingspeak(temperatura, umidade, rele_estado):
     try:
         
-        url = "https://api.thingspeak.com/update?api_key=VEH8ZPQZ5D0750KZ&field1={}&field2={}&field3={}".format(temperatura, umidade, rele_estado)
+        url = "https://api.thingspeak.com/update?api_key=R30GJOUKAHCNAVUI&field1={}&field2={}".format(temperatura, umidade)
         response = urequests.get(url)
         response.close()
-        print("Dados enviados para o ThingSpeak: Temp={}°C, Umidade={}%, Relé={}".format(temperatura, umidade, rele_estado))
+        print("Dados enviados para o ThingSpeak: Temp={}°C, Umidade={}%".format(temperatura, umidade))
     except Exception as e:
         print("Erro ao enviar dados ao ThingSpeak:", e)
-
-def verificar_condicoes(temperatura, umidade):
-    if temperatura > 31 or umidade > 70:
-        RELE_PIN.value(1)
-        print("Relé ligado!")
-        return 1
-    else:
-        RELE_PIN.value(0)
-        print("Relé desligado.")
-        return 0
 
 while True:
     try:
@@ -52,8 +40,7 @@ while True:
         
         print("Temperatura: {}°C, Umidade: {}%".format(temperatura, umidade))
         
-        rele_estado = verificar_condicoes(temperatura, umidade)
-        thingspeak(temperatura, umidade, rele_estado)
+        thingspeak(temperatura, umidade)
         
     except OSError as e:
         print("Erro ao ler o sensor:", e)
